c0a080fd7f1592e54be246c986424c0346f7c0c8
Saveliy Tronza <sparker_95@mail.ru>
2023-01-15 16:27:35 +0100

    Vehicle icon is colorized based on its faciton

--- ProjectRefineMap/scripts/Game/ProjectRefineMap/Map/PR_ActiveMapIconVehicle.c
+++ ProjectRefineMap/scripts/Game/ProjectRefineMap/Map/PR_ActiveMapIconVehicle.c
@@ -4,4 +4,20 @@ class PR_ActiveMapIconVehicleClass : PR_ActiveMapIconClass
 
 class PR_ActiveMapIconVehicle : PR_ActiveMapIcon
 {
+	override protected void UpdateFromReplicatedState()
+	{
+		super.UpdateFromReplicatedState();
+		
+		MapItem mapItem = m_MapDescriptor.Item();
+		MapDescriptorProps props = mapItem.GetProps();
+		
+		// Icon color
+		Color color;
+		Faction faction = GetGame().GetFactionManager().GetFactionByIndex(m_iFactionId);
+		if (faction)
+			color = faction.GetFactionColor();
+		else
+			color = Color.White;
+		props.SetFrontColor(color);
+	}
 }
