79ad09b4050c5f5d3e8cd3bddda04dd48c378996
Saveliy Tronza <sparker_95@mail.ru>
2023-01-18 22:58:46 +0100

    Handling of cases when vehicle is dismounted or vehicle is spawned

--- ProjectRefineMap/scripts/Game/ProjectRefineMap/Map/PR_ActiveMapIconVehicle.c
+++ ProjectRefineMap/scripts/Game/ProjectRefineMap/Map/PR_ActiveMapIconVehicle.c
@@ -20,4 +20,31 @@ class PR_ActiveMapIconVehicle : PR_ActiveMapIcon
 			color = Color.White;
 		props.SetFrontColor(color);
 	}
+	
+	
+	override protected void UpdateFaction(FactionAffiliationComponent factionAffiliationComponent, bool init)
+	{
+		SCR_FactionAffiliationComponent scrf = SCR_FactionAffiliationComponent.Cast(factionAffiliationComponent);
+		
+		FactionManager fm = GetGame().GetFactionManager();
+		
+		if (!scrf)
+		{
+			Faction faction = factionAffiliationComponent.GetAffiliatedFaction();
+			m_iFactionId = fm.GetFactionIndex(faction);
+			return;
+		}
+		
+		Faction prevFaction = scrf.GetPreviousAffiliatedFaction();
+		Faction faction = scrf.GetAffiliatedFaction();
+		
+		if (init)
+			m_iFactionId = fm.GetFactionIndex(scrf.GetDefaultAffiliatedFaction());	// If it's just created, visible to default faction
+		else if (faction)
+			m_iFactionId = fm.GetFactionIndex(faction);		// If it has new faction, visible to players of the new faction
+		else if (prevFaction)
+			m_iFactionId = fm.GetFactionIndex(prevFaction);	// If it has no new faction (was dismounted), but has previous faction, visible to previous faction
+		else
+			m_iFactionId = -1;
+	}
 }
