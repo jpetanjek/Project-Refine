d94ea8ff3fdc8be93074170b3fd5849627a20dd8
Saveliy Tronza <sparker_95@mail.ru>
2023-02-19 21:42:41 +0100

    Allow taking hats, backpacks and boots

--- ProjectRefine/scripts/Game/ProjectRefine/Inventory/SCR_InventoryMenuUI.c
+++ ProjectRefine/scripts/Game/ProjectRefine/Inventory/SCR_InventoryMenuUI.c
@@ -19,9 +19,22 @@ modded class SCR_InventoryMenuUI
 	
 	override void Action_Drop()
 	{
+		// Check if the item can be dragged or replaced
 		InventoryItemComponent itemComp;
 		if (m_pSelectedSlotUI)
 			itemComp = m_pSelectedSlotUI.GetInventoryItemComponent();
+		
+		if (itemComp && !CanTakeOrDropItem(itemComp.GetOwner()))
+		{
+			ResetHighlightsOnAvailableStorages();
+			return;
+		}
+		
+		// Check if destination can be replaced
+		// We don't want to drag a backpack into a slot with radio backpack and thus drop the radio backpack
+		itemComp = null;
+		if (m_pFocusedSlotUI)
+			itemComp = m_pFocusedSlotUI.GetInventoryItemComponent();
 		
 		if (itemComp && !CanTakeOrDropItem(itemComp.GetOwner()))
 		{
@@ -33,12 +46,22 @@ modded class SCR_InventoryMenuUI
 		return;
 	}
 	
-	bool CanTakeOrDropItem(IEntity item)
+	static bool CanTakeOrDropItem(IEntity item)
 	{
 		if (item.FindComponent(BaseRadioComponent))	// Keep your hands off that radio
 			return false;
-		else if (item.FindComponent(BaseLoadoutClothComponent)) // Can't manipulate uniforms
-			return false;
+		
+		BaseLoadoutClothComponent clothComp = BaseLoadoutClothComponent.Cast(item.FindComponent(BaseLoadoutClothComponent));
+		if (clothComp)
+		{
+			LoadoutAreaType areaType = clothComp.GetAreaType();
+			if (LoadoutBackpackArea.Cast(areaType) ||	// You can take a backpack,
+				LoadoutHeadCoverArea.Cast(areaType) ||	// a hat
+				LoadoutBootsArea.Cast(areaType))		// and boots
+				return true;
+			else
+				return false;	// Keep hands off jackets and pants! We don't do war crimes here!
+		}
 		
 		return true;
 	}
